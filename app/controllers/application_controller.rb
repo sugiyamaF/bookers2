bae543941fb6fb3cc8dc089e8c55b28865be1ea8
EC2 Default User <ec2-user@ip-172-31-29-203.ec2.internal>
2022-08-31 14:53:05 +0000

    56-37

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,9 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resouce)
       root_path
     end
+    
+    add_flash_types :success, :info, :warning, :danger
+    
 
     protected
 
